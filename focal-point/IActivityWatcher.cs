a6a2850e7dd592067d9866b5e59efe891201ce57
Curtis Badke <curtis@badkid.ca>
2013-03-04 16:30:03 -0700

    Shiny new WPF app template

---
+++ focal-point/IActivityWatcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace focal_point
+{
+    public interface IActivityWatcher
+    {
+        void ActivityStarted(DateTime completionTime);
+        void ActivityStopped();
+    }
+}

8abd4be0f6adbb830f1eb7551092a274abdbb6fe
Curtis Badke <curtis@badkid.ca>
2013-03-05 21:36:55 -0700

    Add basic viewmodel and ReactiveUI bindings to window.

--- focal-point/MainWindow.xaml.cs
+++ focal-point/MainWindow.xaml.cs
@@ -12,17 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReactiveUI;
+using ReactiveUI.Xaml;
 
-namespace WpfApplication1
+namespace FocalPoint
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<PomodoroViewModel>
     {
         public MainWindow()
         {
+            ViewModel = new PomodoroViewModel();
+            InitializeComponent();
+
+            this.BindCommand(ViewModel, vm => vm.StartSession);
             InitializeComponent();
         }
+
+        public PomodoroViewModel ViewModel
+        {
+            get { return (PomodoroViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(PomodoroViewModel),typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (PomodoroViewModel)value; }
+        }
+
     }
 }
---
+++ focal-point/PomodoroViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+using ReactiveUI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocalPoint
+{
+    public class PomodoroViewModel : ReactiveObject
+    {
+        private bool _Running;
+        public bool Running
+        {
+            get { return _Running; }
+            set { this.RaiseAndSetIfChanged(value); }
+        }
+
+        public ReactiveCommand StartSession { get; protected set; }
+
+        public PomodoroViewModel()
+        {
+            var canStartSession = this.WhenAny(vm => vm.Running, running => !running.Value);
+            StartSession = new ReactiveCommand(canStartSession);
+            StartSession.Subscribe(_ => this.Running = !this.Running);
+        }
+    }
+}

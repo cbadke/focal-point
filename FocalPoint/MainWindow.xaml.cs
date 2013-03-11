f7c8c8d28c1ff5bc0c4b5a140ca608619e591df4
Curtis Badke <curtis@badkid.ca>
2013-03-10 19:29:04 -0600

    Wire in ability to move window

--- FocalPoint/MainWindow.xaml.cs
+++ FocalPoint/MainWindow.xaml.cs
@@ -59,5 +59,11 @@ namespace FocalPoint
         {
             this.Close();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
     }
 }

41218aeb24fe514052540f558e38928faeaad0be
Curtis Badke <curtis@badkid.ca>
2013-03-16 19:50:25 -0600

    Load plugins from configured path.
    Able to remove all Lync plugin refs from main app!

--- FocalPoint/PluginManager.cs
+++ FocalPoint/PluginManager.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Reflection;
-using FocalPoint.Lync2013Plugin;
 using FocalPoint.SDK;
 
 namespace FocalPoint
@@ -15,12 +16,44 @@ namespace FocalPoint
         {
             _plugins = new List<IPlugin>();
 
-            var asm = Assembly.LoadFrom("FocalPoint.Lync2013Plugin.dll");
-            var factoryType = asm.GetType(asm.GetName().Name + ".Factory");
-            var cInfo = factoryType.GetConstructor(new Type[] { });
+            var pluginDirectory = GetPluginDirectory();
 
-            if (cInfo != null)
+            Directory.GetFiles(pluginDirectory, "*.dll").ToList().ForEach(RegisterPlugin);
+        }
+
+        private static string GetPluginDirectory()
+        {
+            var pluginDirectory = ConfigurationManager.AppSettings["pluginDirectory"];
+
+            if (!Path.IsPathRooted(pluginDirectory))
             {
+                var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+                assemblyPath = new Uri(assemblyPath).LocalPath;
+
+                pluginDirectory = Path.Combine(assemblyPath, pluginDirectory);
+            }
+
+            return pluginDirectory;
+        }
+
+        public IEnumerable<ISessionWatcher> Subscribers
+        {
+            get { return _plugins.Select(p => p.SessionWatcher); }
+        }
+
+        private void RegisterPlugin(string filePath)
+        {
+            try
+            {
+                var asm = Assembly.LoadFrom(filePath);
+                var factoryType = asm.GetType(asm.GetName().Name + ".Factory");
+
+                if (factoryType == null) return;
+
+                var cInfo = factoryType.GetConstructor(new Type[] { });
+
+                if (cInfo == null) return;
+
                 var pluginFactory = cInfo.Invoke(null) as FocalPoint.SDK.IFactory;
 
                 if (pluginFactory != null)
@@ -28,11 +61,9 @@ namespace FocalPoint
                     _plugins.Add(pluginFactory.Create());
                 }
             }
-        }
-
-        public IEnumerable<ISessionWatcher> Subscribers
-        {
-            get { return _plugins.Select(p => p.SessionWatcher); }
+            catch (Exception)
+            {
+            }
         }
     }
 }
